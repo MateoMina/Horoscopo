84717d4029b6f070e59a0f03922b9f6b76680b9f
MateoMina <mateominaclavijo12@gmail.com>
2024-10-10 23:15:33 -0500

    Signos controller

--- api-json/index.js
+++ api-json/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
-const {urlencoded, json} = require('express');
+const { urlencoded, json } = require('express');
 const router = require('./routes/signos.routes.js');
 const cors = require('cors');
 
 const app = express();
 
-app.use(urlencoded({extended: true}))
-app.use(json())
+// Configuración para aceptar datos en formato URL y JSON
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
-app.use(cors())
+// Configuración de CORS para aceptar solicitudes desde tu frontend en Vercel
+app.use(cors({
+    origin: 'https://proyecthoroscopoapi.vercel.app'  // Cambia esto a tu URL de frontend en Vercel
+}));
+
+// Manejar solicitudes preflight OPTIONS
+app.options('*', cors());
+
+// Usar el router para todas las rutas que comiencen con '/v1/signos'
 app.use('/v1/signos', router);
 
-app.listen(4000, ()=>{
-    console.log('listening at port 4000');
-})
+// Escuchar en el puerto asignado por Vercel o en el puerto 4000 en local
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
